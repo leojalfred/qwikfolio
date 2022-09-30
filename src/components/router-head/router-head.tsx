cbbb5dd0dadcb02dd93d0ba88d15eab9b2b05cf7
Leo Alfred <leojalfred@gmail.com>
2022-09-29 22:14:45 -0500

    Creates initial star scroll demo

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,12 +1,12 @@
-import { component$ } from '@builder.io/qwik';
-import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
+import { component$ } from '@builder.io/qwik'
+import { useDocumentHead, useLocation } from '@builder.io/qwik-city'
 
 /**
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
-  const head = useDocumentHead();
-  const loc = useLocation();
+  const head = useDocumentHead()
+  const loc = useLocation()
 
   return (
     <>
@@ -22,22 +22,29 @@ export const RouterHead = component$(() => {
         href="https://fonts.googleapis.com/css2?family=Poppins&amp;display=swap"
         rel="stylesheet"
       />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
 
       <meta property="og:site_name" content="Qwik" />
       <meta name="twitter:site" content="@QwikDev" />
       <meta name="twitter:title" content="Qwik" />
 
-      {head.meta.map((m) => (
+      {head.meta.map(m => (
         <meta {...m} />
       ))}
 
-      {head.links.map((l) => (
+      {head.links.map(l => (
         <link {...l} />
       ))}
 
-      {head.styles.map((s) => (
+      {head.styles.map(s => (
         <style {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
     </>
-  );
-});
+  )
+})
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,20 +1,106 @@
-import { component$, Slot } from '@builder.io/qwik';
-import Header from '../components/header/header';
+import {
+  component$ /* Slot */,
+  useClientEffect$,
+  useRef,
+  useStore
+} from '@builder.io/qwik'
+import * as THREE from 'three'
+// import Header from '../components/header/header';
+
+export let camera: THREE.PerspectiveCamera,
+  scene: THREE.Scene,
+  renderer: THREE.WebGLRenderer
+export const stars: THREE.Mesh<
+  THREE.SphereGeometry,
+  THREE.MeshBasicMaterial
+>[] = []
+
+export function init() {
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    1000
+  )
+  camera.position.z = 5
+
+  scene = new THREE.Scene()
+
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+}
+
+export function addSphere() {
+  for (let z = -1000; z < 1000; z += 5) {
+    const geometry = new THREE.SphereGeometry(0.5, 32, 32)
+    const material = new THREE.MeshBasicMaterial({ color: 0xffffff })
+    const sphere = new THREE.Mesh(geometry, material)
+
+    sphere.position.x = Math.random() * 1000 - 500
+    sphere.position.y = Math.random() * 1000 - 500
+    sphere.position.z = z
+    sphere.scale.x = sphere.scale.y = 2
+
+    scene.add(sphere)
+    stars.push(sphere)
+  }
+}
+
+export function render() {
+  renderer.render(scene, camera)
+}
 
 export default component$(() => {
+  useClientEffect$(() => {
+    init()
+    addSphere()
+    render()
+  })
+
+  // TODO handle window resize
+
+  const main = useRef()
+  const state = useStore({
+    lastScrollTop: 0,
+    scrollDifference: 0
+  })
+
   return (
-    <>
-      <main>
-        <Header />
-        <section>
-          <Slot />
-        </section>
-      </main>
-      <footer>
-        <a href="https://www.builder.io/" target="_blank">
-          Made with ♡ by Builder.io
-        </a>
-      </footer>
-    </>
-  );
-});
+    // <>
+    //   <main>
+    //     <Header />
+    //     <section>
+    //       <Slot />
+    //     </section>
+    //   </main>
+    //   <footer>
+    //     <a href="https://www.builder.io/" target="_blank">
+    //       Made with ♡ by Builder.io
+    //     </a>
+    //   </footer>
+    // </>
+    <main
+      ref={main}
+      onScroll$={() => {
+        if (main.current) {
+          const st = main.current.scrollTop
+          state.scrollDifference = st - state.lastScrollTop
+          requestAnimationFrame(() => {
+            renderer.render(scene, camera)
+
+            for (let i = 0; i < stars.length; i++) {
+              const star = stars[i]
+              star.position.z += state.scrollDifference / 10
+              if (star.position.z > 1000) star.position.z -= 2000
+              else if (star.position.z < -1000) star.position.z += 2000
+            }
+          })
+          state.lastScrollTop = st <= 0 ? 0 : st
+        }
+      }}
+    >
+      <div></div>
+    </main>
+  )
+})
